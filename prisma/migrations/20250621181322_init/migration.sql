2d80ac41131342e08733313cb4227e91a74fcade
wasilewsky <wasilewzki@gmail.com>
2025-06-30 12:02:08 +0200

    fix: add name to migration.sql

--- prisma/migrations/20250621181322_init/migration.sql
+++ prisma/migrations/20250621181322_init/migration.sql
@@ -1,6 +1,7 @@
 -- CreateTable
 CREATE TABLE "User" (
     "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
     "email" TEXT NOT NULL,
     "password" TEXT NOT NULL,
     "role" TEXT NOT NULL DEFAULT 'user',
